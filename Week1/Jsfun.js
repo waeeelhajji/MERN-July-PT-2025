bf59d0a24e715ebffede750e0e4179b1af95fa68
waeeelhajji <waelhajji.contact@gmail.com>
2025-07-14 08:02:23 +0100

    Week1

---
+++ Week1/Jsfun.js
@@ -0,0 +1,45 @@
+// console.log("hello")
+
+// var MERNStack = "Ninjas"
+
+// console.log(MERNStack)
+
+// var name = "Kais"
+// function SayHi() {
+//     let name = "alice"
+//     console.log(name)
+// }
+
+// SayHi()
+// console.log(name)
+
+// const paradigm = "OOP"
+// paradigm = 75
+// console.log(paradigm)
+// const array = ['horse🐎', 'dog🐕', 'fish🐟', 'cat🐈', 'bird🐦'];
+// for (var index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element)
+
+// }
+// console.log(index)
+
+
+
+const a = [10, 20, 30]
+const b = [...a]
+
+b.push(10000)
+
+console.log(a)
+console.log(b)
+
+
+
+function HElloForNEWMERN() {
+    console.log("Hi")
+}
+
+const HelloNewFun = () => {
+    console.log("new Hi")
+}
